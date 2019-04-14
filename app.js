c7187db1bea751381ac5d0c2be48f54004d9dbd8
Khomesh <kk9328843@gmail.com>
2019-04-14 15:24:09 +0530

    registration, login form and logger

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var fs = require('fs');     //built in module
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
+var multer = require('multer');
+// var upload = multer({dest: './uploads'});      for multer(handling image uploads)
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+// var db = mongoose.connection;            database variable
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,7 +25,24 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
-app.set('view engine', 'pug' );
+app.set('view engine', 'pug' );     //changing the view engine to 'pug'
+
+app.use((req, res, next) => {     
+  var now = new Date().toString();  //getting the current data in the string format
+
+  //storing all the data in a variable for printing into a log file
+  var log = `${now}: ${req.method} ${req.url}`;
+  // console.log(log);      for printing in the command prompt
+  fs.appendFile('server.log',log+ '\n', (err) => {
+    if(err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+
+  //when we do not pass next() then the page will never finish loading because then middleware doesn't call next
+  next();
+});
+
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//for the pandit-registration page
+router.get('/registration-pandit', (req, res, next) => {
+  res.render('registration-pandit', {title:'Register'});
+  next();
+});
+
+//for the pandit login page
+router.get('/login-pandit', (req, res, next) => {
+  res.render('login-pandit', {title: 'Login'});
+  next();
+});
+
 module.exports = router;
