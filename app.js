ad143fe14cafecce644d976ff0c9a4765e4c04f8
Khomesh <kk9328843@gmail.com>
2019-04-18 12:54:01 +0530

    Initial header and footer added

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// var pageRouter = require('./routes/pages');
 
 var app = express();
 
@@ -53,6 +54,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// app.use('/page', pageRouter);    adding another routing page
+
+// for showing a common error page for undefined pages while routing
+// app.get('*', function(req, res) {
+//   res.render('error');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -69,5 +76,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -6,75 +6,71 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// for home page
+router.get('/home-page', function(req, res, next) {
+  res.render('home-page', { title: 'Home Page' });
+});
+
 //for the pandit-registration page
 router.get('/registration-pandit', (req, res, next) => {
   res.render('registration-pandit', {title:'Register'});
-  next();
 });
 
 //for the pandit login page
 router.get('/login-pandit', (req, res, next) => {
   res.render('login-pandit', {title: 'Login'});
-  next();
 });
 
 //for the billing page
 router.get('/billing-form', (req, res, next) => {
   res.render('billing-form', {title: 'Billing'});
-  next();
 });
 
 //for the contact us page
 router.get('/contact-us', (req, res, next) => {
   res.render('contact-us', {title: 'Contact us'});
-  next();
-});
-
-//for the annaprashan pooja page
-router.get('/annaprashan', (req, res, next) => {
-  res.render('annaprashan', {title: 'Annaprashan Puja'});
-  next();
 });
 
 //for the 'panditji in bhopal' page
 router.get('/pandit-in-bhopal', (req, res, next) => {
   res.render('pandit-in-bhopal', {title: 'Pandit in Bhopal'});
-  next();
 });
 
 //for the sample 'modal' page
 router.get('/modal', (req, res, next) => {
   res.render('modal', {title: 'Sample Modal'});
-  next();
 });
 
 //for the gods/shiv  page
 router.get('/shiv', (req, res, next) => {
   res.render('shiv', {title: 'Shiv Ji'});
-  next();
 });
 router.get('/laxmi', (req, res, next) => {
   res.render('laxmi', {title: 'Laxmi Ji'});
-  next();
 });
 router.get('/vishnu', (req, res, next) => {
   res.render('vishnu', {title: 'Vishnu Ji'});
-  next();
 });
 router.get('/durga', (req, res, next) => {
   res.render('durga', {title: 'Durga ji'});
-  next();
 });
 router.get('/ganesh', (req, res, next) => {
-  res.render('ganesh', {title: 'Ganesh Ji'});
-  next();
+  res.render('ganesh', {pageTitle: 'Ganesh Ji'});
 });
 router.get('/saraswati', (req, res, next) => {
   res.render('saraswati', {title: 'Saraswati Ji'});
-  next();
 });
 
-//for showing a common error page for undefined pages while routing
+//for the Book pooja page
+router.get('/book-puja', (req, res, next) => {
+  res.render('book-puja', {title: 'Book Puja'});
+});
+
+router.get('/bhoomi-puja', (req, res, next) => {
+  res.render('bhoomi-puja', {title: 'Bhoomi Puja'});
+});
+
+// //for showing a common error page for undefined pages while routing
 // app.get('*', function(req, res) {
 //   res.render('error');
 // });
